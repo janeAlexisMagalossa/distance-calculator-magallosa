9c3bc1c65df474b2996721b17c95205be2b37245
janeAlexisMagalossa <jmagallosa2031@wvc.pshs.edu.ph>
2026-09-05 11:28:04 +0800

    Update distanceprogram.py

--- distanceprogram.py
+++ distanceprogram.py
@@ -1,14 +1,14 @@
-# Reflection:
-# Using the math library simplifies code by avoiding complex formulas from scratch.
-# Functions like sqrt() and pow() make calculations direct, fast, and easy to read.
-
+#allows usage of math.sqrt and math.pow
 import math
 
+#sets up the values
 x1 = float(input("Enter x1: "))
 y1 = float(input("Enter y1: "))
 x2 = float(input("Enter x2: "))
 y2 = float(input("Enter y2: "))
 
+#prepares the formula for solving
 distance = math.sqrt(math.pow(x2 - x1, 2) + math.pow(y2 - y1, 2))
 
+#displays the final answer
 print(f"The distance between the two points is: {distance:.2f}")
